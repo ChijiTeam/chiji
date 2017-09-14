83b8dd3fe949f3ce032acb5248c9cd528a7c7118
zhangliguang <zhangliguang@chyogame.com>
2017-09-14 20:51:57 +0800

    增加移动

---
+++ Chiji/Assets/Scripts/Battle/BattleCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCamera : MonoBehaviour {
+	public Transform followTarget;
+	public Vector3 followPos;
+//	public Vector3 followAngles;
+	// Use this for initialization
+	void Start () {
+//		transform.eulerAngles = followAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (followTarget != null) {
+			transform.position = followTarget.position + followPos;
+//			transform.eulerAngles = followAngles;
+			transform.LookAt(followTarget);
+		}
+	}
+}
