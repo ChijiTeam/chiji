754ac8357e939c65b98f2125780c9029fad07b66
zhusu <zhusu@chyogame.com>
2017-09-18 00:15:20 +0800

    进房间

---
+++ Chiji/Assets/Scripts/Building/BuildingTop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTop : MonoBehaviour {
+
+	public MeshRenderer mesh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider)  
+	{  
+		print("Enter ::: "+this.name);  
+//		this.gameObject.SetActive (false);
+		mesh.enabled = false;
+
+
+	}  
+	void OnTriggerExit(Collider collider)  
+	{  
+		print("out ::: "+this.name);  
+//		this.gameObject.SetActive (true);
+
+		mesh.enabled = true;
+	} 
+}
